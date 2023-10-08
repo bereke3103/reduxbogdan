c6a4be625b13e6351f1394739e16f02a61ee67f7
Bereke Sakan <bereke3103@mail.ru>
2023-10-08 22:33:04 +0600

    Inital React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-header">
+        <h1>Book Library App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
